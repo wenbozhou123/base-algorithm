ecc3cc9c02bcdd249cf24d410e3c62f4d9ecc920
wenbozhou123 <18721789537@163.com>
2019-09-12 00:15:17 +0800

    change

---
+++ src/main/java/com/algrithm/sort/GuibingSort.java
@@ -0,0 +1,18 @@
+package com.algrithm.sort;
+
+public class GuibingSort {
+
+    private static int[] arr = {2, 0, 9, 1, 6, 7, 4, 12, 10, 22, 34, 1, 19, 21, 2, 5};
+
+    public static void main(String[] args) {
+        int[] result = sort(arr);
+        for (int i : result) {
+            System.out.print(i + ",");
+        }
+    }
+
+    private static int[] sort(int[] arr) {
+
+        return arr;
+    }
+}
